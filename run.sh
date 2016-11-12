dfe819dffdd7822f5524c6784423cfcc452a0e00
tonyzhang1231 <tonyzhang1231@gmail.com>
2016-11-12 10:03:55 -0500

    commit

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 # th CIFAR10_PCANet.lua -max_epochs 100 -learning_rate 0.001 -batch_size 64
 
 
-# to train the CNN run
+# to train the CNN, run
 # th CIFAR10_CNN.lua -max_epochs 100 -learning_rate 0.001 -batch_size 64
 
